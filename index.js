1d5ea3afbca1e285a39f39fa2239b6bbc6ff83e6
Sawyer Silfies <ssilfies17@gmail.com>
2022-03-11 19:02:48 -0800

    Script developed. Inquirer attached and prompts are developed.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const userAnswers = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the Project title?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Give a brief description of the project.',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide detailed instructions and examples for use of the project. Include screenshots if desired/relevant.',
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List collaborators. Include contact links such as GitHub, email, etc.',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Please include a license. "Choosealicense.com" if unsure. ',
+        },
+    ]);
+};
+
